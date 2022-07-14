18fd7140145ca55d4f59120b818ba5cc0897a416
hanzeceng <zeceng.han@pandaminer.com>
2022-07-14 14:38:31 +0800

    11

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { applyMiddleware, createStore,compose } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import reducer from './reducer'
-import thunk from 'thunk';
+import thunk from 'redux-thunk';
 
-const composeEnhance = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSER__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace:true}) || compose;
+const storeEnhance = applyMiddleware(thunk)
+const store = createStore(reducer,composeEnhancers(storeEnhance))
 
-const store = createStore(reducer,applyMiddleware(thunk))
+export default store 
 
